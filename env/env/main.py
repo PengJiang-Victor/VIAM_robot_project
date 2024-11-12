1a0f5d3be053258bde77cfec8a9fd845bc7a71a2
Shengtao Yao <sy3535@nyu.edu>
2024-11-12 14:50:01 -0500

    aa

--- env/env/main.py
+++ env/env/main.py
@@ -1,18 +1,22 @@
+# ---------------------------------------------Lib---------------------------------------------
 import asyncio, math
 from viam.components.base import Base
 from viam.robot.client import RobotClient
 from viam.services.slam import SLAMClient
 
+# ---------------------------------------------Key---------------------------------------------
 API_KEYID = 'aa407263-ffda-44df-a93e-a96d99499d9e'
 API_KEY = 'oyz54e8pbdgs2zrwxv01nfoza2mbrutb'
 API_ADDR = 'machine-main.y3r5oijw6x.viam.cloud'
 SLAM_SERVICE_NAME = 'slam-1'
 BASE = 'viam_base'
 
-import math
-import asyncio
+# ---------------------------------------------Constant---------------------------------------------
+SPIN_VELOCITY=50
+GO_VELOCITY=100
 
-async def move_to_position(base, slam_service, target_x, target_y, target_theta, velocity=100, step_size=150, tolerance=150):
+# ---------------------------------------------Func---------------------------------------------
+async def move_to_position(base, slam_service, target_x, target_y, target_theta, velocity=GO_VELOCITY, step_size=150, tolerance=150):
     while True:
         current_position = await slam_service.get_position()
         current_x = current_position.x
@@ -31,7 +35,7 @@ async def move_to_position(base, slam_service, target_x, target_y, target_theta,
             break
 
         try:
-            await base.spin(velocity=50, angle=int(angle_to_rotate))
+            await base.spin(velocity=SPIN_VELOCITY, angle=int(angle_to_rotate))
         except Exception as e:
             print(f"Error: {e} -- Continue.")
             continue
@@ -43,7 +47,7 @@ async def move_to_position(base, slam_service, target_x, target_y, target_theta,
     final_adj = target_theta - current_theta
     final_adj = (final_adj + 180) % 360 - 180
     print(f"final orientation by {final_adj:.2f}")
-    await base.spin(velocity=50, angle=int(final_adj))
+    await base.spin(velocity=SPIN_VELOCITY, angle=int(final_adj))
     print("Finished.")
 
 async def connect():
@@ -52,19 +56,22 @@ async def connect():
 
 async def move_in_square(base):
     for _ in range(4):
-        await base.move_straight(velocity=100, distance=300)
-        await base.spin(velocity=50, angle=90)
+        await base.move_straight(velocity=GO_VELOCITY, distance=300)
+        await base.spin(velocity=SPIN_VELOCITY, angle=90)
 
+# ---------------------------------------------Main ---------------------------------------------
 async def main():
-    # inti variables.
+    # init variables.
     robot = await connect()
     base = Base.from_robot(robot, BASE)
     slam_service = SLAMClient.from_robot(robot, SLAM_SERVICE_NAME)
 
+    # retreive the original position 
     src_pos = await slam_service.get_position()
-
     for i in range(5): 
         await move_in_square(base)
+        
+        # sleep for a while to manually move robot  
         await asyncio.sleep(5)
         dst_pos = await slam_service.get_position()
         if (abs(dst_pos.x - src_pos.x) < 150) or (abs(dst_pos.y - src_pos.y) < 150) or (abs(dst_pos.theta - src_pos.theta) < 3):
@@ -80,5 +87,6 @@ async def main():
     await robot.close()
     
     
+# ---------------------------------------------Where everything starts...---------------------------------------------
 if __name__ == "__main__":
     asyncio.run(main())
